74a45fcea103bb4ad4abdf2ae3335402d969b698
Tanvir14720 <mashrur15-15720@diu.edu.bd>
2022-07-04 01:38:42 +0600

    accepted

---
+++ Uri 1176.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, j;
+    long long int N[60];
+    N[0] = 0;
+    N[1] = 1;
+    for (i = 0; i < 61; i++)
+        N[i] = N[i - 1] + N[i - 2];
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &j);
+        printf("Fib(%d) = %d\n", j, N[j]);
+    }
+    return 0;
+}
