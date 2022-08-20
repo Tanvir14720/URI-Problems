6c8fc9911d22a0965f726f63bb2e17649839458f
Tanvir14720 <mashrur15-15720@diu.edu.bd>
2022-08-20 19:32:18 +0600

    init

---
+++ Uri 1052.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    if (1 == N)
+        printf("january");
+    else if (2 == N)
+        printf("February");
+    else if (3 == N)
+        printf("March");
+    else if (4 == N)
+        printf("April");
+    else if (5 == N)
+        printf("May");
+    else if (6 == N)
+        printf("June");
+    else if (7 == N)
+        printf("July");
+    else if (8 == N)
+        printf("August");
+    else if (9 == N)
+        printf("September");
+    else if (10 == N)
+        printf("October");
+    else if (11 == N)
+        printf("November");
+    else if (12 == N)
+        printf("December");
+    return 0;
+}
